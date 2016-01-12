ae58c09f67992dc6eec6006a53a3c289fa9f8bc2
fzagato <fzagato@gmail.com>
2016-01-12 00:27:51 -0600

    Initial Commit

---
+++ Parse Object/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parse Object
+//
+//  Created by Francisco Javier Trujillo Lopez on 12/01/16.
+//  Copyright © 2016 FZagato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Parse Object/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Parse Object
+//
+//  Created by Francisco Javier Trujillo Lopez on 12/01/16.
+//  Copyright © 2016 FZagato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
